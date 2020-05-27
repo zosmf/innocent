7a82e7b0c129cef666ad4870d8edf4ba2eca6812
Mission <Mission@lvhsws-imac>
2020-05-27 16:05:12 +0200

    Committing 00 offence from springSeason

--- nittyGritty/src/savant/pad/Rocket.java
+++ nittyGritty/src/savant/pad/Rocket.java
@@ -5,30 +5,37 @@ import java.util.Scanner;
 public class Rocket 
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
         Scanner reader = new Scanner(System.in);
         int age;
         // program code
         System.out.println("Type your age or 0 to quit");
         
-        while (true) {
-            age = Integer.parseInt(reader.nextLine());
+        while (true) 
+		        {
+		            age = Integer.parseInt(reader.nextLine());
+		
+		            if ( (age >= 5 && age <= 85) || age==0) {  // age between 5 AND 85
+		                break;  // end the loop
+		            }
+		
+		            System.out.println("You are lying!");
+		            if (age < 5) {
+		                System.out.println("You are so young that you cannot know how to write!");
+		            } else {  // that means age is over 85
+		                System.out.println("You are so old that you cannot know how to use a computer!");
+		            }
+		
+		            System.out.println("Type your age again: ");
+		        }
 
-            if ( (age >= 5 && age <= 85) || age==0) {  // age between 5 AND 85
-                break;  // end the loop
-            }
-
-            System.out.println("You are lying!");
-            if (age < 5) {
-                System.out.println("You are so young that you cannot know how to write!");
-            } else {  // that means age is over 85
-                System.out.println("You are so old that you cannot know how to use a computer!");
-            }
-
-            System.out.println("Type your age again: ");
+        if (age == 0) {
+            System.out.println("You quit!");
+        } else {  // that means age is over 85
+            System.out.println("Your age is " + age);
         }
-
-        System.out.println("Your age is " + age);
-    }
+        
+     }
 
 }
